b21512c38886092821061ac5766cadc526bac087
MichelT <micheltallon@gmail.com>
2016-09-29 20:21:00 +0000

    Formattage des textes de status

--- model/formatter.js
+++ model/formatter.js
@@ -10,6 +10,58 @@ sap.ui.define([], function () {
         isNotEmpty: function (sValue) {
             if (sValue) { return true};
             return false;
-        }
+        },
+        montant: function (facture, reglement) {
+            // Remvoi le montant de la facture en négatif sinon le règlement en positif
+            var montant = facture || reglement;
+            if (facture) { 
+                montant = -1 * montant 
+            };
+            var value = sap.ui.core.format.NumberFormat.getCurrencyInstance();
+            return value.format(montant);
+        },
+        
+        soldeDisplay : function (montant) {
+            var value = sap.ui.core.format.NumberFormat.getCurrencyInstance();
+            return  "Solde : " + value.format(montant);
+            
+        },
+        isStatusVisible : function (status) {
+            if (typeof status == 'undefined') { 
+                return false;
+            } else if (status=='N') { 
+                return true;
+            } else if (status=='X') {
+                return true;
+            } else if (status=='R') {
+                return true;
+            }
+            ;
+        },
+        isStatusText : function (status) {
+            if (typeof status == 'undefined') { 
+                return null;
+            } else if (status=='N') { 
+                return "Nouvelle facture";
+            } else if (status=='X') {
+                return "Facture réglé";
+            } else if (status=='R') {
+                return "Facture en retard";
+            }
+            ;
+        },
+        isStatusState : function (status) {
+            if (typeof status == 'undefined') { 
+                return "none";
+            } else if (status=='N') { 
+                return "Warning";
+            } else if (status=='X') {
+                return "Success";
+            } else if (status=='R') {
+                return "Error";
+            }
+            ;
+        }        
+
 	};
 });
