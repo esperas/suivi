f51540f87c09f8088f2841e104793b8961e95f6e
MichelT <micheltallon@gmail.com>
2016-10-01 20:49:44 +0000

    Décomposition en nested view

---
+++ controller/Famille.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "../model/formatter",
+    "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, formatter, JSONModel) {
+    "use strict";
+    return Controller.extend("ecole.famille.controller.Suivi", {
+ 
+    });
+});
