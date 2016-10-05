bd1f86a0de04417b4e61a6e3cdeccab1e3e0f1de
esperas <micheltallon@gmail.com>
2016-10-05 11:12:58 +0200

    Navigation par menu, ajout de muni.view dans la page master du composant split

---
+++ controller/Menu.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
+    "use strict";
+    return Controller.extend("ecole.famille.controller.Menu", {
+
+    });
+});
