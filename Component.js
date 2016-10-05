eb534dfe40af2aaa1d5dae6120cd115d440a4d88
esperas <micheltallon@gmail.com>
2016-10-05 11:12:58 +0200

    Ajout d'un contrôle de cohérence sur le login, redirection vers les pages "nologin"

--- Component.js
+++ Component.js
@@ -1,27 +1,54 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
+
     "sap/ui/model/resource/ResourceModel"
-], function (UIComponent, JSONModel, ResourceModel) {
+], function (UIComponent, JSONModel,  ResourceModel) {
     "use strict";
     return UIComponent.extend("ecole.famille.Component", {
         metadata : {
             manifest: "json"
         },
+
+        checkFile : function ( address ) {
+
+            var href = window.location.origin + window.location.pathname;  // On retirer la chaine de recherche
+            if (href.search("index.html")!=-1) {
+                href = href.slice(0, href.search("index.html"));
+            }
+            address =  href + address;
+            console.log(href);
+            var client = new XMLHttpRequest();
+            window.cool = this.getRouter();
+            client.addEventListener("load", this.returnStatus);
+            client.open( "GET", address );
+            client.send();
+        },
+
+        returnStatus : function ( oEvent ) {
+            if ( oEvent.currentTarget.status === 200 ) {
+                console.log( 'file exists!' );
+            }
+            else {
+                console.log( 'file does not exist! status: ' + status );
+                window.cool.getTargets().display("nologin");
+            }
+        },
+
         init : function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
-            // set data model
-            /*var oModel = new sap.ui.model.json.JSONModel("json/data.json")
-            .attachRequestCompleted( function() {
-                console.log("Load Json local", oModel);
-                sap.ui.getCore().setModel(oModel,"famille");
-                oModel.refresh();
-                return oModel;
-            } );*/
+
+            // create the views based on the url/hash
+            this.getRouter().initialize();
+
             var parent = jQuery.sap.getUriParameters().get("parent");
             parent = "json/" + parent + ".json";
             //Mise à jour du model avec les "vrai" données
+
+            // Vérification des
+            this.checkFile(parent);
+
             var oModel = this.getModel("famille");
 
             oModel.loadData(parent);
@@ -33,9 +60,9 @@ sap.ui.define([
             });
             this.setModel(i18nModel, "i18n");
 
-
-            // create the views based on the url/hash
-            this.getRouter().initialize();
         }
+
+
+
     });
 });
