fcdbfffa5c5110a30fdce150a14bc9e533c476c2
michel <micheltallon@gmail.com>
2016-10-14 20:04:30 +0200

    Réorganisation du code, sortie des vérifications de fichier de component.js et ajout d'un fichier file.js

--- Component.js
+++ Component.js
@@ -2,38 +2,15 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel",
-	"sap/ui/Device"
-], function (UIComponent, JSONModel,  ResourceModel, Device) {
+	"sap/ui/Device",
+    "./model/file"
+], function (UIComponent, JSONModel,  ResourceModel, Device, file) {
     "use strict";
     return UIComponent.extend("ecole.famille.Component", {
         metadata : {
             manifest: "json"
         },
-
-        checkFile : function ( address ) {
-
-            var href = window.location.origin + window.location.pathname;  // On retirer la chaine de recherche
-            if (href.search("index.html")!=-1) {
-                href = href.slice(0, href.search("index.html"));
-            }
-            address =  href + address;
-            console.log(href);
-            var client = new XMLHttpRequest();
-            window.cool = this.getRouter();
-            client.addEventListener("load", this.returnStatus);
-            client.open( "GET", address );
-            client.send();
-        },
-
-        returnStatus : function ( oEvent ) {
-            if ( oEvent.currentTarget.status === 200 ) {
-                console.log( 'file exists!' );
-            }
-            else {
-                console.log( 'file does not exist! status: ' + status );
-                window.cool.getTargets().display("nologin");
-            }
-        },
+        file : file,
 
         init : function () {
             // call the init function of the parent
@@ -55,7 +32,9 @@ sap.ui.define([
             //Mise à jour du model avec les "vrai" données
 
             // Vérification des
-            this.checkFile(parent);
+            this.file.checkFile(parent);
+
+            window.cool = this.getRouter();
 
             var oModel = this.getModel("famille");
 
---
+++ model/file.js
@@ -0,0 +1,30 @@
+sap.ui.define([], function () {
+    "use strict";
+
+    return {
+
+        checkFile : function ( address ) {
+
+            var href = window.location.origin + window.location.pathname;  // On retirer la chaine de recherche
+            if (href.search("index.html")!=-1) {
+                href = href.slice(0, href.search("index.html"));
+            }
+            address =  href + address;
+            var client = new XMLHttpRequest();
+            client.addEventListener("load", this.returnStatus);
+            client.open( "GET", address );
+            client.send();
+        },
+        returnStatus : function ( oEvent ) {
+            if ( oEvent.currentTarget.status === 200 ) {
+                console.log( 'file exists!' );
+            }
+            else {
+                console.log( 'file does not exist! status: ' + status );
+                window.cool.getTargets().display("nologin");
+            }
+        },
+
+
+    };
+});
