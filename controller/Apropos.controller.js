46c1d3dff76da33504808160cbf5cac4d28d488d
esperas <micheltallon@gmail.com>
2016-10-14 20:04:30 +0200

    Correction du bouton back pour navigation par téléphone

--- controller/Apropos.controller.js
+++ controller/Apropos.controller.js
@@ -6,7 +6,11 @@ sap.ui.define([
 ], function (Controller, MessageToast, formatter, JSONModel) {
     "use strict";
     return Controller.extend("ecole.famille.controller.Apropos", {
-
+        navBack : function() {
+            var oSplitApp = this.getView().getParent().getParent();
+            var oMaster = oSplitApp.getMasterPages()[0];
+            oSplitApp.toMaster(oMaster, "slide");
+        }
       
     });
 });
